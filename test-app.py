7e40ab0da1461cd03e49c5e787380425f5e7e0c5
3BLABLAB <rk3b.0215@gmail.com>
2025-01-15 14:22:34 +0900

    Add: 似ているポケモンを返せる

--- test-app.py
+++ test-app.py
@@ -6,20 +6,21 @@ from tensorflow.keras.preprocessing import image
 
 import numpy as np
 
-
 classes = ["線状の","粒々の","もくもくとした","ふわっとした","うろこ状の", "霧状の"]
+pokes = ["ドータクン", "マッギョ", "メタモン", "ピッピ", "プロトーガ", "レックウザ"]
 image_size = 200
+image_resize = 300
 
 UPLOAD_FOLDER = "uploads"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./uploads/")
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 model = load_model('./model.h5')#学習済みモデルをロード
-
+model_poke = load_model('./model_poke.h5')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -39,17 +40,36 @@ def upload_file():
             img = image.img_to_array(img)
             data = np.array([img])
             
+            img_resized = image.load_img(filepath, target_size=(image_resize,image_resize,3))
+            img_resized = image.img_to_array(img_resized)
+            data_poke = np.array([img_resized])
+            
+            
             #変換したデータをモデルに渡して予測する
             result = model.predict(data)[0]
-            predicted = result.argmax()
-            ans_per = float(result[predicted]*100)
+            result_poke= model_poke.predict(data_poke)[0]
+            predicted = result.argmax()#正解クラス
+            predicted_poke = result_poke.argmax()
+            accuracy_percentage = result[predicted] * 1# 正解クラスに対する確率
+            accuracy_percentage_poke = result_poke[predicted_poke]*1
+            ans_per = float(result[predicted])
+            ans_per_poke = float(result_poke[predicted_poke])
 
-            pred_answer = "これは 【 " + classes[predicted] + "雲 】 です"
+            pred_answer = (
+            f"これは 【  {classes[predicted]} 雲 】 です"
+            #"適合率は{accuracy_percentage:.2f}%です"
+            "一番似ているポケモンは 【{ pokes[predicted_poke]} 】です"
+            #"適合率は{accuracy_percentage_poke:.2f}%です"
+            )
   
         return render_template("test-index.html",answer=pred_answer)
 
     return render_template("test-index.html",answer="")
 
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    os.path.abspath(UPLOAD_FOLDER + filename)
+    return render_template("result.html", img="uploadss" + filename + ".png")
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
